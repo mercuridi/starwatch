9a2b1bf02b6c044df1cadfa662f37837a6810e22
dinay <trainee.dinay.halai@sigmalabs.co.uk>
2025-07-28 16:35:36 +0100

    Added moon phase functionality

--- src/extract_astronomy_data.py
+++ src/extract_astronomy_data.py
@@ -9,11 +9,46 @@ load_dotenv('src/.env')
 user_pass = f"{os.environ.get('applicationId')}:{os.environ.get("applicationSecret")}"
 auth_string = base64.b64encode(user_pass.encode()).decode()
 headers = {
-    'Authorization': f'Basic {auth_string}'
+    'Authorization': f'Basic {auth_string}',
+    'Content-Type': 'application/json'
+
 }
 
-url = "https://api.astronomyapi.com/api/v2/bodies"
+region_latitude = 40.7128
+region_longitude = -74.0060
+date = '2025-07-28'
 
-response = requests.get(url, headers=headers)
+moon_phase_payload = {
+    "observer": {
+        "latitude": region_latitude,
+        "longitude": region_longitude,
+        "date": date
+    },
+    "view": {
+        "type": "landscape-simple",
+        "orientation": "north-up"
+    },
+    "style": {
+        "moonStyle": "default",
+        "backgroundStyle": "solid",
+        "backgroundColor": "black",
+        "headingColor": "white",
+        "textColor": "white"
+    }
+}
 
-print(response.json())
+moon_phase_url = "https://api.astronomyapi.com/api/v2/studio/moon-phase"
+
+
+def get_moon_phase_image():
+
+    response = requests.post(
+        moon_phase_url, headers=headers, json=moon_phase_payload)
+    if response.status_code == 200:
+        print(response.json()['data']['imageUrl'])
+    else:
+        print(f'Error {response.status_code}')
+
+
+if __name__ == "__main__":
+    get_moon_phase_image()
