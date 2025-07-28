156ec9fc761e0d3ba9acb6682c4d08270f84a846
dinay <trainee.dinay.halai@sigmalabs.co.uk>
2025-07-28 18:13:28 +0100

    Updated gitignore

--- src/extract_astronomy_data.py
+++ src/extract_astronomy_data.py
@@ -1,7 +1,8 @@
+'''Extracts moon phase and planetary body information from Astronomy API'''
 import os
 import base64
-import requests
 import json
+import requests
 from dotenv import load_dotenv
 
 load_dotenv('src/.env')
@@ -12,22 +13,18 @@ auth_string = base64.b64encode(user_pass.encode()).decode()
 headers = {
     'Authorization': f'Basic {auth_string}',
     'Content-Type': 'application/json'
-
 }
 
-region_latitude = 40.7128
-region_longitude = -74.0060
-date = '2025-07-28'
+REGION_LATITUDE = 40.7128
+REGION_LONGITIDE = -74.0060
+DATE = '2025-07-28'
 
 
-moon_phase_url = "https://api.astronomyapi.com/api/v2/studio/moon-phase"
-
-
-def get_planetary_positions(latitude, longitude, date, time):
-
+def get_planetary_positions(latitude, longitude, date, time, header):
+    '''Gets information on planetary bodies and saves it to a file as JSON'''
     planetary_positions_url = f'https://api.astronomyapi.com/api/v2/bodies/positions?latitude={latitude}&longitude={longitude}&elevation=0&from_date={date}&to_date={date}&time={time}'
 
-    response = requests.get(planetary_positions_url, headers=headers)
+    response = requests.get(planetary_positions_url, headers=header)
 
     if response.status_code == 200:
         directory = "src/extracted_data"
@@ -39,8 +36,8 @@ def get_planetary_positions(latitude, longitude, date, time):
         print(f'Error {response.status_code}, {response.json()}')
 
 
-def get_moon_phase_image(latitude, longitude, date):
-
+def get_moon_phase_image(latitude, longitude, date, header):
+    '''Generates a moon phase image based on latitude and longitude and saves as png'''
     moon_phase_url = "https://api.astronomyapi.com/api/v2/studio/moon-phase"
 
     moon_phase_payload = {
@@ -62,7 +59,7 @@ def get_moon_phase_image(latitude, longitude, date):
         }
     }
     response = requests.post(
-        moon_phase_url, headers=headers, json=moon_phase_payload)
+        moon_phase_url, headers=header, json=moon_phase_payload)
     if response.status_code == 200:
         image_url = response.json()['data']['imageUrl']
         img_data = requests.get(image_url).content
@@ -76,5 +73,5 @@ def get_moon_phase_image(latitude, longitude, date):
 
 if __name__ == "__main__":
     get_planetary_positions(
-        region_latitude, region_longitude, date, '00:00:00')
-    get_moon_phase_image(region_latitude, region_longitude, date)
+        REGION_LATITUDE, REGION_LONGITIDE, DATE, '00:00:00', headers)
+    get_moon_phase_image(REGION_LATITUDE, REGION_LONGITIDE, DATE, headers)
