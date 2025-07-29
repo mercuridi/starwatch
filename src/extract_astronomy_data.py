5285b3350dfba1718462c9180cc71d5eb87c1fd9
mercuridi <kaibarberharris@protonmail.com>
2025-07-29 14:59:17 +0100

    refactoring globals

--- src/extract_astronomy_data.py
+++ src/extract_astronomy_data.py
@@ -7,18 +7,18 @@ import requests
 from dotenv import load_dotenv
 
 def get_planetary_positions(
-        lat: float,
-        lon: float,
+        coordinates: dict[str:float],
         date: str,
         time: str,
-        header: dict[str:str]
+        header: dict[str:str],
+        data_filepath: str
     ) -> None:
     '''Gets information on planetary bodies and saves it to a file as JSON'''
 
     # String concatenation method to ensure one line
     # is broken into multiple for readability.
     # '+' is optional but included for clarity
-    planetary_positions_url = get_positions_url(lat, lon, date, time)
+    planetary_positions_url = get_positions_url(coordinates, date, time)
 
     response = requests.get(
         planetary_positions_url,
@@ -26,13 +26,13 @@ def get_planetary_positions(
         timeout=20
     )
 
-    handle_response(response)
+    handle_response(response, data_filepath)
 
 
-def handle_response(response: requests.Response) -> None:
+def handle_response(response: requests.Response, data_filepath: str) -> None:
     """Handles the API response; prints errors on fail and dumps to JSON on success"""
     if response.status_code == 200:
-        json_path = handle_dump_path()
+        json_path = handle_dump_path(data_filepath)
         dump_json_data(response, json_path)
     else:
         print(f'Error {response.status_code}, {response.json()}')
@@ -44,22 +44,22 @@ def dump_json_data(response: requests.Response, json_path: str) -> None:
         json.dump(response.json(), f, indent=2)
 
 
-def handle_dump_path() -> str:
+def handle_dump_path(data_filepath: str) -> str:
     """Ensures the data directory exists and returns a path to it"""
-    os.makedirs(DATA_FILE_PATH, exist_ok=True)
+    os.makedirs(data_filepath, exist_ok=True)
     json_path = os.path.join(
-        DATA_FILE_PATH,
+        data_filepath,
         'planetary_data.json'
     )
     return json_path
 
 
-def get_positions_url(lat: float, lon: float, date: str, time: str) -> str:
+def get_positions_url(coordinates: dict[str:float], date: str, time: str) -> str:
     """Constructs the API endpoint for the planetary positions data"""
     planetary_positions_url: str = (
         "https://api.astronomyapi.com/api/v2/bodies/positions"
-        + f"?latitude={lat}"
-        + f"&longitude={lon}"
+        + f"?latitude={coordinates["lat"]}"
+        + f"&longitude={coordinates["lon"]}"
         + "&elevation=0"
         + f"&from_date={date}"
         + f"&to_date={date}"
@@ -73,7 +73,7 @@ if __name__ == "__main__":
     load_dotenv()
 
     # Folder path for the json output
-    DATA_FILE_PATH = '../data/'
+    DATA_FILEPATH = '../data/'
 
     # Authentication method for Astronomy API
     USER_PASS = f"{os.environ.get('applicationId')}:{os.environ.get('applicationSecret')}"
@@ -93,9 +93,12 @@ if __name__ == "__main__":
     TIME = '00:00:00'
 
     get_planetary_positions(
-        REGION_LATITUDE,
-        REGION_LONGITUDE,
+        {
+            "lat": REGION_LATITUDE,
+            "lon": REGION_LONGITUDE
+        },
         DATE,
         TIME,
-        HEADERS
+        HEADERS,
+        DATA_FILEPATH
     )
--- test/test_extract_astronomy_data.py
+++ test/test_extract_astronomy_data.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from src.extract_astronomy_data import DATA_FILE_PATH, USER_PASS, AUTH_STRING, HEADERS, DATE, TIME
+from src.extract_astronomy_data import DATA_FILEPATH, USER_PASS, AUTH_STRING, HEADERS, DATE, TIME
 
 def test_dummy():
     assert True
