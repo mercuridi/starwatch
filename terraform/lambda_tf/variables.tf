a4b8d9a17071cf5b4ba76353aec2014bb62c1a1b
mercuridi <kaibarberharris@protonmail.com>
2025-07-31 14:47:13 +0100

    i think OK??

---
+++ terraform/lambda_tf/variables.tf
@@ -0,0 +1,54 @@
+variable "function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region to use"  
+  type = string
+}
+
+variable "ecr_name" {
+  description = "ECR repository name"
+  type        = string
+}
+
+variable "vpc_id" {
+    description = "ID for the VPC to use"
+    type = string
+}
+
+variable "DB_HOST" {
+  description = "IP to access the RDS"
+  type        = string
+}
+variable "DB_PORT" {
+  description = "Port for MS SQLServer"
+  type        = string
+}
+variable "DB_USER" {
+  description = "Group-specific username for RDS"
+  type        = string
+}
+variable "DB_PASSWORD" {
+  description = "Group-specific password for RDS"
+  type        = string
+}
+variable "DB_NAME" {
+  description = "Database name for RDS"
+  type        = string
+}
+variable "DB_SCHEMA" {
+  description = "Group-specific schema for RDS"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "Private subnet IDs to host Lambda"
+  type        = list(string)
+}
+
+variable "public_subnet_ids" {
+  description = "Public subnet IDs for NAT Gateway"
+  type        = list(string)
+}
