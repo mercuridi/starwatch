ed5e85a88e96e8f52a0afb6c12820cfba6a36b7b
dinay <trainee.dinay.halai@sigmalabs.co.uk>
2025-08-04 16:36:48 +0100

    Added moon phase dashboard script

---
+++ src/moon_phase_extract.py
@@ -0,0 +1,60 @@
+import requests
+import os
+from datetime import datetime
+
+import src.astronomy_utils
+
+
+def get_moon_phase_image(latitude, longitude, date, headers, save_path='data/moon_phase.jpg'):
+
+    moon_phase_url = "https://api.astronomyapi.com/api/v2/studio/moon-phase"
+
+    moon_phase_payload = {
+        "observer": {
+            "latitude": latitude,
+            "longitude": longitude,
+            "date": date
+        },
+        "view": {
+            "type": "landscape-simple",
+            "orientation": "north-up"
+        },
+        "style": {
+            "moonStyle": "default",
+            "backgroundStyle": "solid",
+            "backgroundColor": "black",
+            "headingColor": "white",
+            "textColor": "white"
+        }
+    }
+
+    try:
+        response = requests.post(
+            moon_phase_url, headers=headers, json=moon_phase_payload)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"[Error] Failed to fetch moon phase image URL: {e}")
+        return
+
+    try:
+        image_url = response.json()['data']['imageUrl']
+        img_data = requests.get(image_url).content
+
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+        with open(save_path, 'wb') as f:
+            f.write(img_data)
+
+        print(f"[Success] Moon phase image saved to {save_path}")
+
+    except Exception as e:
+        print(f"[Error] Failed to download or save moon phase image: {e}")
+
+
+if __name__ == "__main__":
+    img = get_moon_phase_image(
+        +51.30,
+        -00.05,
+        '2004-03-03',
+        src.astronomy_utils.make_request_headers()
+    )
