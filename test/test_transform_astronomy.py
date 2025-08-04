3b3e14ae4caa77dfb9a41117b9938f7dbdb621e0
mercuridi <kaibarberharris@protonmail.com>
2025-08-04 14:07:57 +0100

    set up testing

--- test/test_transform_astronomy.py
+++ test/test_transform_astronomy.py
@@ -1,2 +1,14 @@
 # pylint: skip-file
+import json
 
+import pytest
+from unittest.mock import MagicMock, Mock, patch
+
+
+@pytest.fixture
+def dummy_data():
+    with open("data/astronomy_test_data.py") as f:
+        return json.load(f, encoding="utf8")
+
+def test_check_data_in_tables():
+    pass
