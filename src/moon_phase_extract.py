9c948b87c29589ed35fcc1b492c51471a3e17eea
dinay <trainee.dinay.halai@sigmalabs.co.uk>
2025-08-05 11:24:18 +0100

    Added review changes - img returned and url now a parameter

--- src/moon_phase_extract.py
+++ src/moon_phase_extract.py
@@ -1,6 +1,6 @@
 ''' 
 Generates a moon phase image using the Astronomy API based on a specific region
-Saves this image to ../data
+Returns the image
 '''
 
 # pylint:disable=import-error
@@ -16,12 +16,11 @@ def get_moon_phase_image(
         longitude: str,
         date: str,
         headers: dict,
-        save_path='data/moon_phase.jpg'
+        url: str,
 ) -> None:
     '''
     Uses the Astronomy API to generate a moon phase image using given coordinates
     '''
-    moon_phase_url = "https://api.astronomyapi.com/api/v2/studio/moon-phase"
 
     moon_phase_payload = {
         "observer": {
@@ -45,40 +44,22 @@ def get_moon_phase_image(
     # Post request to get moon phase image
     try:
         response = requests.post(
-            moon_phase_url,
+            url,
             headers=headers,
             json=moon_phase_payload,
-            timeout=300
+            timeout=60
         )
         response.raise_for_status()
     except requests.RequestException as e:
         print(f"Error Failed to fetch moon phase image URL: {e}")
         return
 
-    # Convert & save image url from post request to jpg
+    # Convert & return image url from post request to jpg
     try:
         image_url = response.json()['data']['imageUrl']
-        img_data = requests.get(image_url, timeout=300).content
+        img_data = requests.get(image_url, timeout=30).content
 
-        os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-        with open(save_path, 'wb') as f:
-            f.write(img_data)
+        return img_data
 
     except Exception as e:
         print(f"Error Failed to download or save moon phase image: {e}")
-
-
-if __name__ == "__main__":
-
-    TEST_LATITUDE = 51.30
-    TEST_LONGITUDE = -0.05
-    TEST_DATE = datetime.now().date()
-
-    # Test image
-    get_moon_phase_image(
-        TEST_LATITUDE,
-        TEST_LONGITUDE,
-        TEST_DATE,
-        src.astronomy_utils.make_request_headers()
-    )
